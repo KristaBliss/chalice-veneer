1f1377d37bf170f1834d276f68b450a67629a563
kjaye <26447239+KristaBliss@users.noreply.github.com>
2022-03-16 15:41:20 -0400

    add check for if a ServiceBlueprint is already registered. If true, make new Blueprint and reset instantiated routes. This will allow copying sub_blueprints and routes from one ServiceBlueprint to another without importing copy or deepcopy

--- chalice_veneer/service_blueprint.py
+++ chalice_veneer/service_blueprint.py
@@ -18,7 +18,8 @@ class ServiceBlueprint:
         cors: Optional[CORSConfig] = None,
         inherit_cors: Optional[bool] = True,
     ):
-        self.blueprint = Blueprint(import_name or __name__)
+        self.import_name = import_name or __name__
+        self.blueprint = Blueprint(self.import_name)
         self.routes = routes or []
         self._instantiated_routes = []
         self.sub_blueprints = sub_blueprints or []
@@ -28,15 +29,23 @@ class ServiceBlueprint:
         self.inherit_authorizer = inherit_authorizer
         self.cors = cors
         self.inherit_cors = inherit_cors
+        self._registered = False
 
     def propagate(
         self,
         authorizer: Optional[Union[CognitoUserPoolAuthorizer, IAMAuthorizer]] = None,
         cors: Optional[CORSConfig] = None,
     ):
+        if self._registered:
+            # If attempting to register a blueprint again through another blueprint, make new blueprint object
+            self.blueprint = Blueprint(self.import_name)
+            # Instantiated routes can only be appended to, so on re-registering, reset so no old routes are added
+            self._instantiated_routes = []
+
         if self.inherit_authorizer:
             self.authorizer = authorizer
         if self.inherit_cors:
             self.cors = cors
         for route in self.routes:
             self._instantiated_routes.append(route(self.blueprint, self.authorizer, self.cors))
+        self._registered = True
